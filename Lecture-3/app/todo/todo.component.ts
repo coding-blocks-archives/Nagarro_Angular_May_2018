3ddfc239fb2074843e97a5d5d715916e38faac53
aayusharora <aayush113002@gmail.com>
2018-05-08 20:37:51 +0530

    Added Lecture 3

---
+++ Lecture-3/app/todo/todo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  @Input() todo;
+  @Output() result = new EventEmitter<string>();
+  add(item: string) {
+    this.todo.push(item);
+    this.result.emit(this.todo);
+    console.log(this.todo);
+  }
+}
